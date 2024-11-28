86f093a4c2e5c94ae2824fd9c993369514006d9f
Ibna Masud Rana Parvej <35460533+Masud16@users.noreply.github.com>
2024-11-28 18:41:38 +0600

    Added crossOrigin

--- src/main/java/com/admindashboard/e_commerce/e_commerce/authorization/controller/AuthenticationController.java
+++ src/main/java/com/admindashboard/e_commerce/e_commerce/authorization/controller/AuthenticationController.java
@@ -9,13 +9,11 @@ import com.admindashboard.e_commerce.e_commerce.response.MessageResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 public class AuthenticationController {
 
