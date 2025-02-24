5e036c8291166bd7dab058c844bcbfdca02cf16c
Nazim_Uddin <nazimuddin223t@gmail.com>
2025-02-24 22:44:46 +0600

    update district ,sub-district,division bug

--- src/main/java/com/admindashboard/e_commerce/e_commerce/model/addressComp/DivisionController.java
+++ src/main/java/com/admindashboard/e_commerce/e_commerce/model/addressComp/DivisionController.java
@@ -6,24 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Controller
 @RequestMapping("/api/division")
 public class DivisionController {
+
     @Autowired
     private AddressService addressService;
 
     @PostMapping("/add")
-    public ResponseEntity<?> addDivision(@RequestBody AddressDto addressDto)
-    {
+    public ResponseEntity<?> addDivision(@RequestBody AddressDto addressDto) {
+        System.out.println(addressDto);
         try {
             return ResponseEntity.ok(addressService.addDivision(addressDto));
-        }catch (Exception ex){
+        } catch (Exception ex) {
             return new ResponseEntity<>(new MessageResponse("Internal error or bad request.", ResponseType.E), HttpStatus.BAD_REQUEST);
         }
     }
