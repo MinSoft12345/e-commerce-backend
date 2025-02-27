3cfbe462d29e090a112d941bbafef484da86a9be
maeen <maeenuddin1971@gmail.com>
2025-02-27 22:37:24 +0800

    Price, currency & category entity are added

---
+++ src/main/java/com/admindashboard/e_commerce/e_commerce/model/productManagementModule/productComponent/Price.java
@@ -0,0 +1,54 @@
+package com.admindashboard.e_commerce.e_commerce.model.productManagementModule.productComponent;
+
+import com.admindashboard.e_commerce.e_commerce.authorization.User;
+import com.admindashboard.e_commerce.e_commerce.model.masterData.Currency;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "EC_CATEGORY")
+public class Price {
+    @Id
+    @GenericGenerator(name = "idGen", strategy = "uuid.hex")
+    @GeneratedValue(generator = "idGen")
+    private String id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "created_by", nullable = false, updatable = false)
+    private User createdBy;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "updated_by", nullable = false, updatable = true)
+    private User updatedBy;
+
+    private Double amount;
+
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Currency currency;
+
+    private Double discount;
+
+    private Date effectiveDate;
+    private Date expiryDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+}
