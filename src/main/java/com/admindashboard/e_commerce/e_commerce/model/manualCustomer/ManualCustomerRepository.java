51c0af22d85a19ba31d50197f5984de9f90811b6
Ibna Masud Rana Parvej <35460533+Masud16@users.noreply.github.com>
2025-03-06 23:21:57 +0600

    add Manual Customer API updated for not adding exist customer

--- src/main/java/com/admindashboard/e_commerce/e_commerce/model/manualCustomer/ManualCustomerRepository.java
+++ src/main/java/com/admindashboard/e_commerce/e_commerce/model/manualCustomer/ManualCustomerRepository.java
@@ -5,4 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ManualCustomerRepository extends JpaRepository<ManualCustomer,String> {
+    ManualCustomer findByPhoneNumber(String phoneNumber);
 }
